839eeaa3d2a5f30ddfc85aa34a3f01ac81ad7a12
Luna <nore0404@gmail.com>
2022-04-21 11:12:28 +0200

    added layout and logo

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,59 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<section class="layout">
+		<div class="sidebar-top">
+			<img style="height: 3vh; width: 6.5vh; padding-top: 3vh;" src="https://cdn.discordapp.com/attachments/640641733151162388/966421281111150612/logo_white.png" alt="Vision">
+		</div>
+		<div class="searchbar">2</div>
+		<div class="navigation-top">3</div>
+		<div class="sidebar-right">4</div>
+		<div class="sidebar-bottom">5</div>
+		<div class="secondary-field">6</div>
+		<div class="main-field">
+
+		</div>
+		<div class="sidebar-right-bottom">8</div>
+	  </section>
 </main>
 
 <style>
+	.layout {
+  		width: 100%;
+  		height: 96vh;
+
+  		display: grid;
+  		grid:
+    	"sidebar-top searchbar navigation-top sidebar-right" 10%
+    	"sidebar-bottom secondary-field main-field sidebar-right-bottom" 1fr
+		/ 7% 15% 1fr 15%;
+  		gap: 1px;
+	}
+
+	.sidebar-top { grid-area: sidebar-top; 
+		background-color: #23272a;
+	}
+	.searchbar { grid-area: searchbar; 
+		background-color: #23272a;
+	}
+	.navigation-top { grid-area: navigation-top; 
+		background-color: #2c2f33;
+	}
+	.sidebar-right { grid-area: sidebar-right; 
+		background-color: #23272a;
+	}
+	.sidebar-bottom { grid-area: sidebar-bottom; 
+		background-color: #23272a;
+	}
+	.secondary-field { grid-area: secondary-field; 
+		background-color: #23272a;
+	}
+	.main-field { grid-area: main-field; 
+		background-color: #2c2f33;
+	}
+	.sidebar-right-bottom { grid-area: sidebar-right-bottom; 
+		background-color: #23272a;
+	}
 	main {
 		text-align: center;
 		padding: 1em;
